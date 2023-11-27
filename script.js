18740ae04fd0bdebe782d8648b110b09723a3a9e
coolland <131864752+coollandmain@users.noreply.github.com>
2023-11-27 22:07:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,17 @@ function initializePage() {
 }
 
 function showHome() {
-    // Use getElementsByClassName to get all elements with the specified class
-    var homeContentElements = document.getElementsByClassName('home-content');
-
-    // Loop through the elements and set their display property
-    for (var i = 0; i < homeContentElements.length; i++) {
-        homeContentElements[i].style.display = 'block';
-    }
-
-    // Hide Coolland SMP and Events sections
+    document.getElementById('home-title').style.display = 'block';
+    document.getElementById('home-content').style.display = 'block';
     document.getElementById('coolland-smp').style.display = 'none';
     document.getElementById('coolland-events').style.display = 'none';
 }
+
+function showEvents() {
+    document.getElementById('home-title').style.display = 'none';
+    document.getElementById('home-content').style.display = 'none';
+    document.getElementById('coolland-smp').style.display = 'none';
+    document.getElementById('coolland-events').style.display = 'block';
 
 
 function toggleSettings() {
