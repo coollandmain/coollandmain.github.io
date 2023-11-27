268dce15688e51e9abb4462bc25c31acfa2b2673
coolland <131864752+coollandmain@users.noreply.github.com>
2023-11-27 22:14:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,16 +7,6 @@ function initializePage() {
 function showHome() {
     document.getElementById('home-title').style.display = 'block';
     document.getElementById('home-content').style.display = 'block';
-    document.getElementById('home-content-1').style.display = 'block';
-    document.getElementById('home-content-2').style.display = 'block';
-    document.getElementById('home-content-3').style.display = 'block';
-    document.getElementById('home-content-4').style.display = 'block';
-    document.getElementById('home-content-5').style.display = 'block';
-    document.getElementById('home-content-6').style.display = 'block';
-    document.getElementById('home-content-7').style.display = 'block';
-    document.getElementById('home-content-8').style.display = 'block';
-    document.getElementById('home-content-9').style.display = 'block';
-    document.getElementById('home-content-10').style.display = 'block';
     document.getElementById('coolland-smp').style.display = 'none';
     document.getElementById('coolland-events').style.display = 'none';
 }
@@ -24,19 +14,9 @@ function showHome() {
 function showEvents() {
     document.getElementById('home-title').style.display = 'none';
     document.getElementById('home-content').style.display = 'none';
-    document.getElementById('home-content-1').style.display = 'none';
-    document.getElementById('home-content-2').style.display = 'none';
-    document.getElementById('home-content-3').style.display = 'none';
-    document.getElementById('home-content-4').style.display = 'none';
-    document.getElementById('home-content-5').style.display = 'none';
-    document.getElementById('home-content-6').style.display = 'none';
-    document.getElementById('home-content-7').style.display = 'none';
-    document.getElementById('home-content-8').style.display = 'none';
-    document.getElementById('home-content-9').style.display = 'none';
-    document.getElementById('home-content-10').style.display = 'none';
     document.getElementById('coolland-smp').style.display = 'none';
     document.getElementById('coolland-events').style.display = 'block';
-
+}
 
 function toggleSettings() {
     var settingsPopup = document.getElementById('settings-popup');
