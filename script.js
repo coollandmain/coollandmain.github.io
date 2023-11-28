fb323067964847230e30471bfdee8d8b25cc9204
coolland <131864752+coollandmain@users.noreply.github.com>
2023-11-28 19:47:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,81 +13,6 @@ function showEvents() {
     document.getElementById('home').style.display = 'none';
     document.getElementById('coolland-events').style.display = 'block';
 }
-function showMessage() {
-    // Hide other sections
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('coolland-events').style.display = 'none';
-
-    // Show the "Message" section
-    document.getElementById('message').style.display = 'block';
-
-    // Initialize messaging section
-    // Initialize messaging section
-    initializeMessaging();
-}
-
-function initializeMessaging() {
-    // Clear message container
-    document.getElementById('message-container').innerHTML = '';
-
-    // Set up event listeners or any other initialization for messaging
-}
-
-function sendMessage() {
-    var userMessage = document.getElementById('user-message').value;
-
-    // Display the message in the message container
-    var messageContainer = document.getElementById('message-container');
-    var messageElement = document.createElement('div');
-    messageElement.textContent = userMessage;
-    messageContainer.appendChild(messageElement);
-
-    // Clear the input field
-    document.getElementById('user-message').value = '';
-}
-function register() {
-    // Get the entered username and password for registration
-    var registerUsername = document.getElementById('register-username').value;
-    var registerPassword = document.getElementById('register-password').value;
-
-    // Perform registration logic (you can add server-side validation here)
-    // For simplicity, we'll just log the details for now
-    console.log('Registration:', registerUsername, registerPassword);
-}
-
-function login() {
-    // Get the entered username and password for login
-    var loginUsername = document.getElementById('login-username').value;
-    var loginPassword = document.getElementById('login-password').value;
-
-    // Perform login logic (you can add server-side validation here)
-    // For simplicity, we'll just log the details for now
-    console.log('Login:', loginUsername, loginPassword);
-
-    // Set the username for posting announcements
-    document.getElementById('announcement-username').value = loginUsername;
-}
-
-function postAnnouncement() {
-    // Get the entered username and message for the announcement
-    var announcementUsername = document.getElementById('announcement-username').value;
-    var announcementMessage = document.getElementById('announcement-message').value;
-
-    // Display the announcement in the announcements container
-    var announcementsContainer = document.getElementById('announcements-container');
-    var announcementElement = document.createElement('div');
-    announcementElement.innerHTML = '<h3>' + announcementUsername + '</h3><p>' + announcementMessage + '</p>';
-    announcementsContainer.appendChild(announcementElement);
-}
-
-// Add a function to show the "Message" content
-function showMessage() {
-    // Hide other sections
-    document.getElementById('home').style.display = 'none';
-    document.getElementById('coolland-events').style.display = 'none';
-
-    // Show the "Message" section
-    document.getElementById('message').style.display = 'block';
 
 function toggleSettings() {
     var settingsPopup = document.getElementById('settings-popup');
@@ -123,8 +48,6 @@ document.getElementById('button-home').addEventListener('click', showHome);
 document.getElementById('button-events').addEventListener('click', showEvents);
 document.getElementById('button-home').addEventListener('touchend', showHome);
 document.getElementById('button-events').addEventListener('touchend', showEvents);
-document.getElementById('button-message').addEventListener('click', showMessage);
-document.getElementById('button-message').addEventListener('touchend', showMessage);
 
 
 
